14b42db07799122d4e3fbbc81f1310ad99897e96
it'smepressly <130832755+itspressly@users.noreply.github.com>
2024-05-01 18:26:33 +0200

    first commit

---
+++ my-school-project/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import MeFOrSchool from './assets/MeFOrSchool.jpg'
+function App() {
+ 
+
+  return (
+    <>
+      <div className='contain'>
+        
+        <button></button>
+      <h1>This is me pressly</h1>
+      <img src={MeFOrSchool} 
+      alt="Read this" />
+      <p>Im a student at garlandale high school</p>
+      <a href="https://github.com/itspressly"><button>My Github</button></a>
+    </div>
+    </>
+  )
+}
+
+export default App
